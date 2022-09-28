0ef4c6f7856756f7e85fdb2d2d5310775493813b
Fatih Şen <fatihydrm@outlook.com>
2022-09-29 01:54:24 +0300

    update

--- 10-Mongodb/app.js
+++ 10-Mongodb/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 
@@ -15,13 +14,23 @@ mongoose
 
 app.set("view engine", "ejs");
 
-// app.use((req, res, next) => {
-//   console.log("new request made:");
-//   console.log("host: ", req.hostname);
-//   console.log("path: ", req.path);
-//   console.log("method: ", req.method);
-//   next();
-// });
+// mongoose & mongo tests
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 3",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // middleware & static files
 app.use(express.static("public"));
@@ -33,6 +42,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", {title: "About"});
 });
+
 // blogs routes
 app.get("/blogs", (req, res) => {
   Blog.find()
