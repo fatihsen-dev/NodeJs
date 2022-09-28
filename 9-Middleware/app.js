ea63cbd0ae53b3b9c9def7aae4ccf2966b5da0ff
Fatih Şen <fatihydrm@outlook.com>
2022-09-28 16:32:12 +0300

    update

---
+++ 9-Middleware/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
+
+// middleware & static files
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", {title: "Home", blogs});
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {title: "About"});
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", {title: "Create blog"});
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", {title: "404"});
+});
+
+app.use(express.static("public"));
