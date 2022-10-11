87baf77e761274e1e223152fecea586116f955cc
Fatih Şen <fatihydrm@outlook.com>
2022-10-12 01:17:34 +0300

    form-data

---
+++ 03-Series-ST/01-Temeller/06-form-data/app.js
@@ -0,0 +1,53 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer((request, response) => {
+  if (request.url == "/") {
+    fs.readFile("index.html", (error, html) => {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else if (request.url == "/blogs") {
+    fs.readFile("blogs.html", (error, html) => {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else if ((request.url == "/create", request.method == "POST")) {
+    const data = [];
+    request.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    request.on("end", (chunk) => {
+      const result = Buffer.concat(data).toString();
+      const parsedData = result.split("=")[1];
+
+      fs.appendFile("blogs.txt", parsedData, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          response.statusCode = 302;
+          response.setHeader("Location", "/");
+          response.end();
+        }
+      });
+    });
+  } else if (request.url == "/create") {
+    fs.readFile("create.html", (error, html) => {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else {
+    fs.readFile("404.html", (error, html) => {
+      response.writeHead(404, { "content-type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  }
+});
+
+server.listen(3000);
+
+console.log("Node.js server at port 300");
