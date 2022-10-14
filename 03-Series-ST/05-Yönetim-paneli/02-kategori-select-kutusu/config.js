fe179ea96291711a90fb4544b3528323ff5acda9
Fatih Şen <fatihydrm@outlook.com>
2022-10-14 18:29:34 +0300

    kategori select kutusu

---
+++ 03-Series-ST/05-Yönetim-paneli/02-kategori-select-kutusu/config.js
@@ -0,0 +1,8 @@
+module.exports = config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "102030++",
+    database: "blogapp",
+  },
+};
---
+++ 03-Series-ST/05-Yönetim-paneli/02-kategori-select-kutusu/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+
+app.use(express.static("node_modules"));
+app.use(express.static("public"));
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
+app.listen(3000, () => {
+  console.log("Listining on port 3000");
+});
