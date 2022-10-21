e7509837768d947ddcdfd379b5d62edb1bc87718
Fatih Şen <fatihydrm@outlook.com>
2022-10-22 01:52:01 +0300

    update

---
+++ 03-Series-ST/05-Yönetim-paneli/03-blog-kaydının-eklenmesi/data/db.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+const config = require("../config");
+
+const connection = mysql.createConnection(config.db);
+
+connection.connect((err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("mysql server bağlantısı yapıldı");
+});
+
+module.exports = connection.promise();
