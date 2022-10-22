25b8bcd7278a42a781d32fe2896e7374395227e4
Fatih Şen <fatihydrm@outlook.com>
2022-10-22 20:34:04 +0300

    html editörü

---
+++ 03-Series-ST/05-Yönetim-paneli/13-html-editörü/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+
+app.use(express.static("node_modules"));
+app.use(express.static("public"));
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
+app.listen(3000, () => {
+   console.log("Listining on port http://localhost:3000/");
+});
