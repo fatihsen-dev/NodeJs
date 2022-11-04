3b23c60fbc323bff74624c0e530190ea893b1720
Fatih Şen <fatihydrm@outlook.com>
2022-11-04 14:05:13 +0300

    find,add,update,delete

--- 03-Series-ST/09-shopapp/routes/products.js
+++ 03-Series-ST/09-shopapp/routes/products.js
@@ -6,12 +6,12 @@ const router = express.Router();
 // get all
 router.get("/", async (req, res) => {
    try {
-      // const products = await Product.find();
+      const products = await Product.find();
       // const products = await Product.find({ isActive: true });
       // const products = await Product.find({ isActive: true }).select({ name: 1, price: 1 });
-      const products = await Product.find({ isActive: true })
-         .limit(3)
-         .select({ name: 1, price: 1 });
+      // const products = await Product.find({ isActive: true })
+      //    .limit(3)
+      //    .select({ name: 1, price: 1 });
 
       res.send(products);
    } catch (error) {
@@ -46,22 +46,68 @@ router.post("/", async (req, res) => {
 
 // update
 router.put("/:id", async (req, res) => {
-   const id = req.params.id;
-   const product = products.filter((product) => product.id == id);
-
-   if (product.length == 0) {
-      return res.status(404).send({ message: "Ürün bulunamadı" });
-   }
-
    const { error } = productValidate(req.body);
 
    if (error) {
       return res.status(400).send({ message: error.details[0].message });
    }
-   product[0].name = req.body.name;
-   product[0].price = req.body.price;
 
-   return res.send(product[0]);
+   try {
+      // try {
+      //    const prıduct = await Product.findByIdAndUpdate(
+      //       req.params.id,
+      //       {
+      //          $set: {
+      //             name: req.body.name,
+      //             price: req.body.price,
+      //             description: req.body.description,
+      //             imageUrl: req.body.imageUrl,
+      //             isActive: req.body.isActive,
+      //          },
+      //       },
+      //       { new: true }
+      //    );
+      //    return res.send(prıduct);
+      // } catch (error) {
+      //    console.log(error);
+      //    return res.status(500).send({ message: "Güncelleme başarısız" });
+      // }
+
+      // try {
+      //    const prıduct = await Product.update(
+      //       { _id: req.params.id },
+      //       {
+      //          $set: {
+      //             name: req.body.name,
+      //             price: req.body.price,
+      //             description: req.body.description,
+      //             imageUrl: req.body.imageUrl,
+      //             isActive: req.body.isActive,
+      //          },
+      //       }
+      //    );
+      //    res.send(prıduct);
+      // } catch (error) {
+      //    console.log(error);
+      // }
+
+      const product = await Product.findById(req.params.id);
+      product.name = req.body.name;
+      product.price = req.body.price;
+      product.description = req.body.description;
+      product.imageUrl = req.body.imageUrl;
+      product.isActive = req.body.isActive;
+      try {
+         const updateProduct = await product.save();
+         return res.send(updateProduct);
+      } catch (error) {
+         console.log(error);
+         return res.status(500).send({ message: "Güncelleme başarısız" });
+      }
+   } catch (error) {
+      console.log(error);
+      return res.status(404).send({ message: "Ürün bulunamadı" });
+   }
 });
 
 // get one
@@ -75,21 +121,22 @@ router.get("/:id", async (req, res) => {
 });
 
 // delete
-router.delete("/:id", (req, res) => {
-   const id = req.params.id;
-   const product = products.filter((product) => product.id == id);
+router.delete("/:id", async (req, res) => {
+   // try {
+   //    const result = await Product.deleteOne({ _id: req.params.id });
 
-   if (product.length == 0) {
+   //    return res.send(result);
+   // } catch (error) {
+   //    console.log(error);
+   //    return res.status(404).send({ message: "Silme Başarısız" });
+   // }
+
+   const product = await Product.findByIdAndDelete(req.params.id);
+
+   if (!product) {
       return res.status(404).send({ message: "Ürün bulunamadı" });
    }
-
-   const index = products.indexOf(product[0]);
-
-   if (index == -1) {
-      return res.status(404).send({ message: "İndex Hatalı" });
-   }
-   products.splice(index, 1);
-   return res.status(200).send({ message: "Ürün silindi" });
+   res.send(product);
 });
 
 export default router;
