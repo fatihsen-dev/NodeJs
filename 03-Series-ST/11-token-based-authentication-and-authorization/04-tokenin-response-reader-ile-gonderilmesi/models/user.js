b307d65797a572c5fa72f51f2c24d003515d67ac
Fatih Şen <fatihydrm@outlook.com>
2022-11-06 23:51:43 +0300

    Token'in Response Header ile Gönderilmesi

--- 03-Series-ST/11-token-based-authentication-and-authorization/04-tokenin-response-reader-ile-gonderilmesi/models/user.js
+++ 03-Series-ST/11-token-based-authentication-and-authorization/04-tokenin-response-reader-ile-gonderilmesi/models/user.js
@@ -3,43 +3,49 @@ import jwt from "jsonwebtoken";
 import Joi from "joi";
 
 const userSchema = mongoose.Schema(
-   {
-      name: {
-         type: String,
-         required: true,
-      },
-      email: {
-         type: String,
-         required: true,
-         unique: true,
-      },
-      password: {
-         type: String,
-         required: true,
-      },
-   },
-   { timestamps: true }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: Boolean,
+  },
+  { timestamps: true }
 );
 
 export const userValidate = (user) => {
-   const schema = new Joi.object({
-      name: Joi.string().min(3).max(50).required(),
-      email: Joi.string().min(3).max(50).required().email(),
-      password: Joi.string().min(6).required(),
-   });
-   return schema.validate(user);
+  const schema = new Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(3).max(50).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(user);
 };
 
 export const loginValidate = (user) => {
-   const schema = new Joi.object({
-      email: Joi.string().min(3).max(50).required().email(),
-      password: Joi.string().min(6).required(),
-   });
-   return schema.validate(user);
+  const schema = new Joi.object({
+    email: Joi.string().min(3).max(50).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(user);
 };
 
-userSchema.methods.createAuthToken = (user) => {
-   return jwt.sign({ _id: user._id }, "jwtPricateKey");
+userSchema.methods.createAuthToken = function () {
+  const decodedToken = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    "jwtPricateKey"
+  );
+  console.log(decodedToken);
+  return decodedToken;
 };
 
 export default mongoose.model("User", userSchema);
