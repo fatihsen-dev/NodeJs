972502d6f07a588c320a73a4fb675312882b3626
Fatih Şen <fatihydrm@outlook.com>
2022-11-08 00:39:50 +0300

    Production Settings

--- 03-Series-ST/11-token-based-authentication-and-authorization/06-nodejs-publish/01-production-settings/index.js
+++ 03-Series-ST/11-token-based-authentication-and-authorization/06-nodejs-publish/01-production-settings/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./startup/routes.js";
 import db from "./startup/db.js";
+import productions from "./startup/productions.js";
 dotenv.config();
 const app = express();
 
+if (process.env.Node_ENV == "production") {
+  productions(app);
+}
 routes(app);
-
 db();
 
-app.listen(process.env.PORT || 5000, async () => {
-  console.log(`Listening on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  console.log(`Listening on port ${port}`);
 });
